60cfcc2644c3dc961dcc30d4ab6c4a73c09ba798
kai713 <kairgaliev713@gmail.com>
2025-06-21 23:58:33 +0500

    ONS-45 - исключение для ExpiredJwtException

--- src/main/java/com/kairgaliyev/backendonlineshop/security/JWTAuthFilter.java
+++ src/main/java/com/kairgaliyev/backendonlineshop/security/JWTAuthFilter.java
@@ -5,12 +5,14 @@ import com.kairgaliyev.backendonlineshop.enums.UserRole;
 import com.kairgaliyev.backendonlineshop.repository.UserRepository;
 import com.kairgaliyev.backendonlineshop.service.CustomUserDetailsService;
 import com.kairgaliyev.backendonlineshop.utils.JWTService;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -33,49 +35,61 @@ public class JWTAuthFilter extends OncePerRequestFilter {
     private final UserRepository userRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                    FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        final String authHeader = request.getHeader("Authorization");
+        try {
+            final String authHeader = request.getHeader("Authorization");
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            String jwtToken = authHeader.substring(7);
-            String userEmail = jwtService.extractUsername(jwtToken);
-            Long userId = jwtService.extractUserId(jwtToken);
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String jwtToken = authHeader.substring(7);
+                String userEmail = jwtService.extractUsername(jwtToken);
+                Long userId = jwtService.extractUserId(jwtToken);
 
-            if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = customUserDetailsService.loadUserByUsername(userEmail);
-                if (jwtService.isValidToken(jwtToken, userDetails)) {
-                    UsernamePasswordAuthenticationToken token =
-                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                    token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = customUserDetailsService.loadUserByUsername(userEmail);
+                    if (jwtService.isValidToken(jwtToken, userDetails)) {
+                        UsernamePasswordAuthenticationToken token =
+                                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                    SecurityContext context = SecurityContextHolder.createEmptyContext();
-                    context.setAuthentication(token);
-                    SecurityContextHolder.setContext(context);
+                        SecurityContext context = SecurityContextHolder.createEmptyContext();
+                        context.setAuthentication(token);
+                        SecurityContextHolder.setContext(context);
 
-                    request.setAttribute("userId", userId);
-                    filterChain.doFilter(request, response);
-                    return; // Авторизован — больше ничего не нужно
+                        request.setAttribute("userId", userId);
+                        filterChain.doFilter(request, response);
+                        return; // Авторизован — больше ничего не нужно
+                    }
                 }
             }
+
+            // Если не авторизован или токен невалиден — fallback на anonymous guest
+            String uuid = getUuidFromCookies(request);
+
+            if (uuid != null) {
+                userRepository.findByName(uuid).ifPresentOrElse(
+                        user -> setSpringSecurityContext(new CustomUserDetails(user), request),
+                        () -> createGuest(uuid, response, request)
+                );
+            } else {
+                String newUuid = UUID.randomUUID().toString();
+                createGuest(newUuid, response, request);
+            }
+
+            filterChain.doFilter(request, response);
+
+        } catch (ExpiredJwtException e) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.getWriter().write("""
+                    {
+                      "status": 401,
+                      "message": "Expired JWT token",
+                      "timestamp": "%s"
+                    }
+                    """.formatted(LocalDateTime.now()));
         }
-
-        // Если не авторизован или токен невалиден — fallback на anonymous guest
-        String uuid = getUuidFromCookies(request);
-
-        if (uuid != null) {
-            userRepository.findByName(uuid).ifPresentOrElse(
-                    user -> setSpringSecurityContext(new CustomUserDetails(user), request),
-                    () -> createGuest(uuid, response, request)
-            );
-        } else {
-            String newUuid = UUID.randomUUID().toString();
-            createGuest(newUuid, response, request);
-        }
-
-        filterChain.doFilter(request, response);
     }
 
     private String getUuidFromCookies(HttpServletRequest request) {
